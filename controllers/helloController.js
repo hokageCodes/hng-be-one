f3633f1d44161b1f3d9c79a1052a6472fe446759
hokageCodes <hashsenju94@gmail.com>
2024-07-02 16:01:45 +0100

    abeg work naaa

--- controllers/helloController.js
+++ controllers/helloController.js
@@ -13,15 +13,17 @@ exports.sayHello = async (req, res) => {
     clientIp = clientIp.split(',')[0];
   }
   clientIp = clientIp.split(':')[0];
-  console.log(clientIp);
+  console.log(`Client IP: ${clientIp}`);
 
   try {
     // Get location information
     const locationResponse = await axios.get(`https://ipinfo.io/${clientIp}?token=${process.env.IP_TOKEN}`);
+    console.log('Location response data:', locationResponse.data);
     const location = locationResponse.data.city;
 
     // Get weather information from OpenWeatherMap
     const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHER_KEY}&units=metric`);
+    console.log('Weather response data:', weatherResponse.data);
     const temperature = weatherResponse.data.main.temp;
 
     // Prepare response
@@ -33,7 +35,15 @@ exports.sayHello = async (req, res) => {
 
     res.json(response);
   } catch (error) {
-    console.error('Error details:', error.response ? error.response.data : error.message);
+    if (error.response) {
+      console.error('Error response data:', error.response.data);
+      console.error('Error response status:', error.response.status);
+      console.error('Error response headers:', error.response.headers);
+    } else if (error.request) {
+      console.error('Error request:', error.request);
+    } else {
+      console.error('Error message:', error.message);
+    }
     res.status(500).send('An error occurred while processing your request.');
   }
 };
