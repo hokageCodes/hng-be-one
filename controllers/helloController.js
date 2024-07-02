f66477dc70736770cfe748bb3a550edf60cbc808
hokageCodes <hashsenju94@gmail.com>
2024-07-02 16:33:12 +0100

    abeg work naaa

--- controllers/helloController.js
+++ controllers/helloController.js
@@ -5,6 +5,7 @@ exports.sayHello = async (req, res) => {
   const visitorName = req.query.visitor_name;
 
   if (!visitorName) {
+    console.error("Visitor's name is required");
     return res.status(400).json({ msg: 'Visitor\'s name is required' });
   }
 
@@ -13,17 +14,28 @@ exports.sayHello = async (req, res) => {
     clientIp = clientIp.split(',')[0];
   }
   clientIp = clientIp.split(':').pop();
+
+  // Use a static IP for testing in production
+  if (process.env.NODE_ENV === 'production') {
+    clientIp = '1.1.1.1';
+  }
+
   console.log(`Client IP: ${clientIp}`);
 
   try {
     // Get location information
     const locationResponse = await axios.get(`https://ipinfo.io/${clientIp}?token=${process.env.IP_TOKEN}`);
     console.log('Location response data:', locationResponse.data);
+
     const location = locationResponse.data.city || 'Unknown';
+    if (location === 'Unknown') {
+      console.error('Could not determine location for IP:', clientIp);
+    }
 
     // Get weather information from OpenWeatherMap
     const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHER_KEY}&units=metric`);
     console.log('Weather response data:', weatherResponse.data);
+
     const temperature = weatherResponse.data.main.temp;
 
     // Prepare response
